c160d9f0a4c4edc22e435860ed6a5669a4c4e065
Kirill Trapeznikov <kirill.trapeznikov@str.us>
2022-06-08 00:18:22 +0000

    adding changes to dataset

--- create_dataset.py
+++ create_dataset.py
@@ -1,6 +1,9 @@
 from gaia.data import NCDataConstructor
 
 
+cam4 = "cam4-famip-30m-timestep"
+spcam  = "spcamclbm-nx-16-20m-timestep"
+
 if __name__=="__main__":
-    NCDataConstructor.default_data(split="test")
+    NCDataConstructor.default_data(split="train",  workers  =32, prefix=cam4, train_years=3, save_location=".", cache = ".")
     # NCDataConstructor.default_data(split="train")

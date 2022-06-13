cdcf6236a4457c9c84cc8e927fa2e02bc2a6be3d
Kirill <kirill.trapeznikov@str.us>
2022-06-13 17:19:49 -0400

    more

--- export_model.py
+++ export_model.py
@@ -6,5 +6,5 @@ export_name = "export_model_cam4.pt"
 inputs = ['FLNT', 'FLNS', 'T', 'SOLIN', 'Z3', 'OMEGA', 'FSNS', 'LHFLX', 'FSNT', 'SHFLX', 'U', 'V', 'Q', 'PSL']
 outputs = ['PRECC', 'PTTEND', 'PTEQ', 'PRECT']
 
-export(model_dir,export_name, inputs, outputs)
+export(model_dir, export_name, inputs, outputs)
 

e52e443051c32d73c43a475b568f1c2b22c6cbc7
Kirill <kirill.trapeznikov@str.us>
2022-06-13 17:15:13 -0400

    adding export script

---
+++ export_model.py
@@ -0,0 +1,12 @@
+from gaia.export import export
+
+
+
+model_dir = "lightning_logs_hparam/version_0"
+export_name = "export_model_cam4.pt"
+
+inputs = ['FLNT', 'FLNS', 'T', 'SOLIN', 'Z3', 'OMEGA', 'FSNS', 'LHFLX', 'FSNT', 'SHFLX', 'U', 'V', 'Q', 'PSL']
+outputs = ['PRECC', 'PTTEND', 'PTEQ', 'PRECT']
+
+export(model_dir,export_name, inputs, outputs)
+

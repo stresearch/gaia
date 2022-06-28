a8e1026d19ded2e962a607cf36e44a8593ead08d
Kirill Trapeznikov <kirill.trapeznikov@str.us>
2022-06-28 22:10:35 +0000

    adding ignore

--- gaia/data.py
+++ gaia/data.py
@@ -491,13 +491,13 @@ class NCDataConstructor:
                 ","
             ),
             outputs="PRECT,PRECC,PTEQ,PTTEND".split(","),
-            flatten=split == "train",
-            shuffle = split == "train",
+            flatten= False,
+            shuffle = False,
             subsample_factor=4,
             compute_stats=True,
             cache = os.path.join(cache,split),
             s3_client_kwargs=s3_client_kwargs,
-            time_steps=2,
+            time_steps=0,
         )
 
 

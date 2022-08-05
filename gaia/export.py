7f92b907a98b1ae965d9f43d59284fe1bcf50ba6
ktrapeznikov <ktrapeznikov@gmail.com>
2022-08-05 12:56:54 -0400

    adding support for time step and variabe selection (#44)
    * adding support for time step and variabe selection
    
    * format
    
    * format
    
    * depreciate
    
    * update export model logic
    
    * fix export
    
    Co-authored-by: Kirill <kirill.trapeznikov@str.us>

--- gaia/export.py
+++ gaia/export.py
@@ -1,5 +1,6 @@
 from asyncio.log import logger
 from collections import OrderedDict
+from pathlib import Path
 import torch
 from gaia.models import TrainingModel
 from gaia.training import get_checkpoint_file
@@ -19,23 +20,28 @@ class ModelForExport(torch.nn.Module):
         self.model = training_model.model
 
         if input_order is not None:
-            input_order_index = OrderedDict()
-            i = 0
+            if input_order == list(training_model.hparams.input_index.keys()):
+                logger.info("inputs align... don't need to reorder")
+                self.reorder_input = False
+            else:
 
-            for k in input_order:
-                s, e = training_model.hparams.input_index[k]
-                v_size = e - s
-                input_order_index[k] = (i, i + v_size)
-                i = i + v_size
+                input_order_index = OrderedDict()
+                i = 0
 
-            order = torch.cat(
-                [
-                    torch.arange(*input_order_index[k])
-                    for k in training_model.hparams.input_index.keys()
-                ]
-            )
-            self.register_buffer("input_order", order)
-            self.reorder_input = True
+                for k in input_order:
+                    s, e = training_model.hparams.input_index[k]
+                    v_size = e - s
+                    input_order_index[k] = (i, i + v_size)
+                    i = i + v_size
+
+                order = torch.cat(
+                    [
+                        torch.arange(*input_order_index[k])
+                        for k in training_model.hparams.input_index.keys()
+                    ]
+                )
+                self.register_buffer("input_order", order)
+                self.reorder_input = True
         else:
             self.reorder_input = False
 
@@ -54,6 +60,7 @@ class ModelForExport(torch.nn.Module):
                 self.need_pass_thru = True
             else:
                 raise ValueError(f"{not_in_output} not found in input or output")
+        
 
         if self.need_pass_thru:
             # concat (optionally reordered) input and output and then reorder
@@ -78,8 +85,13 @@ class ModelForExport(torch.nn.Module):
             self.register_buffer("output_order", order)
 
         else:
+            logger.info("dont need to pass thru anything")
+
 
             if output_order is not None:
+                if output_order == list(training_model.hparams.output_index.keys()):
+                    logger.info("outputs align... dont need to reorder")
+                    self.reorder_output = False
                 order = torch.cat(
                     [
                         torch.arange(*training_model.hparams.output_index[k])
@@ -116,7 +128,7 @@ def export(model_dir, export_name, inputs=None, outputs=None):
         map_location="cpu",
     ).eval()
 
-    model_for_export = ModelForExport(model, inputs, outputs).eval()
+    model_for_export = ModelForExport(model, inputs, outputs).eval().requires_grad_(False)
     # TODO dont hard code this
     input_dim = list(model.hparams.input_index.values())[-1][-1]
     example = torch.rand(10, input_dim)
@@ -141,3 +153,12 @@ def export(model_dir, export_name, inputs=None, outputs=None):
     open(os.path.join(model_dir, export_name.replace(".pt", "_io.yaml")), "w").write(
         yaml.dump(dict(inputs=inputs, outputs=outputs), indent=2)
     )
+
+    test_file = (Path(model_dir) / "test_results.json")
+    if test_file.exists():
+        import pandas as pd
+        temp = pd.read_json(test_file).T
+        temp.index = temp.index.str.replace("test_skill_ave_trunc_","")
+        temp.columns = ["metric"]
+        temp.round(3).to_csv(os.path.join(model_dir, export_name.replace(".pt", "_test_skill.csv")))
+
